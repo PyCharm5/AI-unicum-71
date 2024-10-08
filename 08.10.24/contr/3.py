cdd3262a2ef26a63ecd5abf430da10456a9fb0e1
PyCharm5 <ilatelesev2@gmail.com>
2024-10-08 10:38:12 +0700

    Update 3.py

--- 08.10.24/contr/3.py
+++ 08.10.24/contr/3.py
@@ -1,7 +1,7 @@
-import random
-numb = int(input("Введите кол-во чисел:   "))
+import random #Импорт библиотеки
+numb = int(input("Введите кол-во чисел:   ")) #Ввод длины списка
 
-matrix = [random.randint(1,15)for i in range(numb)]
-print(matrix)
-print(max(matrix))
-print(min(matrix))
+matrix = [random.randint(1,15)for i in range(numb)] #Создание случайного списка длины numb
+print(matrix) #Вывод списка
+print(max(matrix)) #Вывод максимального числа в списке
+print(min(matrix)) #Вывод минимального числа в списке
