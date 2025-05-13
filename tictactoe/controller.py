2e91032f8ce9550c5b12882970af2bd6825067bc
PyCharm5 <ilatelesev2@gmail.com>
2025-05-13 08:39:52 +0700

    Update controller.py

--- tictactoe/controller.py
+++ tictactoe/controller.py
@@ -1,35 +1,26 @@
-from typing import Optional
-
-from model import GameModel
-from view import TicTacToeView
-
-
 class TicTacToeController:
-    """Контроллер, связывающий модель и представление"""
-
-    def __init__(self, model: GameModel, view: TicTacToeView):
+    def __init__(self, model, view):
         self.model = model
         self.view = view
 
-        # Устанавливаем обработчики событий
         self.view.set_cell_click_handler(self.handle_cell_click)
         self.view.set_restart_handler(self.handle_restart)
 
-        # Первоначальное обновление представления
         self.update_view()
 
-    def handle_cell_click(self, row: int, col: int) -> None:
-        """Обработчик клика по клетке"""
+    def handle_cell_click(self, row, col):
         if self.model.make_move(row, col):
             self.update_view()
 
-    def handle_restart(self) -> None:
-        """Обработчик перезапуска игры"""
+    def handle_restart(self):
         self.model.reset_game()
         self.update_view()
 
-    def update_view(self) -> None:
-        """Обновить представление на основе текущего состояния модели"""
-        board_state, current_player, game_over, winner = self.model.get_state()
+    def update_view(self):
+        board_state = self.model.board.cells
+        current_player = self.model.current_player
+        game_over = self.model.game_over
+        winner = self.model.winner
+
         self.view.update_board(board_state)
         self.view.update_status(current_player, game_over, winner)
